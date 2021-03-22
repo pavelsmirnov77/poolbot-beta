eca469f32c0de15ad654a8f6b0877c769290b99c
pavelsmirnov77 <64857615+pavelsmirnov77@users.noreply.github.com>
2021-03-22 23:39:59 +0300

    fix for Heroku

--- index.js
+++ index.js
@@ -52,12 +52,7 @@ const Zdrav = mongoose.model('zdrav')
 //database.zdravpoint.forEach(z => new Zdrav(z).save().catch(e => console.log(e)))
 
 //Heroku
-app.get('/', function (request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function () {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
+
 
 /* инлайн херня */
 bot.on('inline_query', query => {
